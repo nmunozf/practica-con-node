1ffb7a303f20998f91680e5acd494613a1dd9f86
NicolasEsteban <33185469+NicolasEsteban@users.noreply.github.com>
2020-11-10 01:04:41 -0300

    termino de actualizar foto
    se termino la funcionalidad de editar imagen

--- aplicacion-con-node/database/db.sql
+++ aplicacion-con-node/database/db.sql
@@ -11,18 +11,19 @@ CREATE TABLE usuarios(
     PRIMARY KEY (id)
 );
 
-
+drop table pertenencias;
 
 CREATE TABLE pertenencias(
     id int(11) not null,
     titulo varchar(150) not null,
     descripcion text not null,
-    usuario_id int(11) not null,
+    usuario_id int(11),
     created_at timestamp not null default current_timestamp,
     constraint fk_usuario foreign key(usuario_id) references usuarios(id)
 );
-
+select * from pertenencias;
 alter table pertenencias add primary key(id);
 alter table pertenencias modify id int(11) not null auto_increment;
 describe usuarios;
 describe pertenencias;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'nicolas123';
